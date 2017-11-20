c2597bc0782774207f2d2031c40335b5c26d0f84
Darren Yang <telunyang@gmail.com>
2017-11-20 23:59:04 +0800

    update CIFAR10.py

--- models/CIFAR10.py
+++ models/CIFAR10.py
@@ -2,6 +2,9 @@ from keras.datasets import cifar10
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, ZeroPadding2D
+from jieba import pool
 
 class CIFAR10:
     def __init__(self):
@@ -22,6 +25,9 @@ class CIFAR10:
             8:'ship',
             9:'truck'
             }
+        
+        #建立 Keras 的 Sequential 模型
+        self.model = Sequential()
     
     def plot_image_labels_prediction(self, images, labels, prediction, idx, num=10):
         fig = plt.gcf()
@@ -43,13 +49,61 @@ class CIFAR10:
         
         #標準化(介於 0-1 之間)
         x_img_train_normalize = self.x_img_train.astype('float32') / 255.0
-        x_label_train_normalize = self.x_label_train.astype('float32') / 255.0
+        x_img_test_normalize = self.x_img_test.astype('float32') / 255.0
         
         #One-Hot編碼
         y_label_train_OneHot = np_utils.to_categorical(self.y_label_train)
         y_label_test_OneHot = np_utils.to_categorical(self.y_label_test)
         
+        #建立卷積層1
+        self.model.add(
+            Conv2D(
+                filters=32,
+                kernel_size=(3,3),
+                input_shape=(32,32,3),
+                activation='relu',
+                padding='same'
+                )
+            )
         
+        #增加 Dropout，避免過度訓練
+        self.model.add(Dropout(rate=0.25))
+        
+        #建立池化層1
+        self.model.add(MaxPooling2D(pool_size=(2,2)))
+        
+        #建立卷積層2
+        self.model.add(
+            Conv2D(
+                filters=64,
+                kernel_size=(3,3),
+                activation='relu',
+                padding='same'
+                )
+            )
+        
+        #增加 Dropout，避免過度訓練
+        self.model.add(Dropout(rate=0.25))
+        
+        #建立池化層2
+        self.model.add(MaxPooling2D(pool_size=(2,2)))
+        
+        #建立平坦層
+        self.model.add(Flatten())
+        
+        #增加 Dropout，避免過度訓練
+        self.model.add(Dropout(rate=0.25))
+        
+        #建立隱藏層
+        self.model.add(Dense(1024, activation='relu'))
+        
+        #增加 Dropout，避免過度訓練
+        self.model.add(Dropout(rate=0.25))
+        
+        #建立輸出層
+        self.model.add(Dense(10, activation='softmax'))
+        
+        print(self.model.summary())
         
 obj = CIFAR10()
 obj.run()
